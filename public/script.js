df6e130cb519b095fa40856254e8a9dc094c9c91
unknown <raj.ankur8@gmail.com>
2020-08-23 14:27:39 +0530

    Zoom clone app

---
+++ public/script.js
@@ -0,0 +1,61 @@
+const socket =io('/')
+const videoGrid =document.getElementById('video-grid');
+const myVideo = document.createElement('video');
+const peers ={}
+myVideo.muted=true;
+const myPeer = new Peer(undefined,{
+     path:'/peerjs',
+    host:'/',
+    port:'434'
+}); 
+let myVideoStream;
+navigator.mediaDevices.getUserMedia(
+   { video:true,
+    audio:true}
+).then(stream=>{
+myVideoStream=stream;
+addVideoStream(myVideo,stream);
+socket.on('user-connect',(userId)=>{
+    newUserConnected(userId,stream);
+})
+myPeer.on('call',call=>{
+    call.answer(stream); 
+    const video = document.createElement('video');
+    
+    call.on('stream', userVideoStream=> {
+        addVideoStream(video,userVideoStream);
+    
+    });
+    call.on('close', () => {
+        video.remove()
+      })
+    
+      peers[userId] = call
+})
+
+});
+socket.on('user-disconnected', userId => {
+    if (peers[userId]) peers[userId].close()
+  })
+myPeer.on('open',id=>{
+    socket.emit('join-room',Room_Id,id);
+})
+
+const newUserConnected=(userId,stream)=>{
+    const call = myPeer.call(userId,stream);
+    const video = document.createElement('video');
+    call.on('stream',(uservideoStream)=>{
+        addVideoStream(video,uservideoStream)
+    })
+    
+console.log("new User connected")
+}
+const addVideoStream = (video,stream)=>{
+
+    video.srcObject=stream;
+    video.addEventListener('loadedmetadata',()=>{
+        video.play()
+    });
+    videoGrid.append(video)    
+}
+// peer.listen('3001')
